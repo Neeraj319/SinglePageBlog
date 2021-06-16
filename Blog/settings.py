89a04ead7bddffa2aed6e320d818400abc39bbe6
Neeraj319 <itskop520@gmail.com>
2021-06-16 20:44:56 +0545

    few changes are made

--- Blog/settings.py
+++ Blog/settings.py
@@ -147,3 +147,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = "static_files"
